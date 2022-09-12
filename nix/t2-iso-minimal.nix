9aa563a37158b86a990c61334b2d41ab9d04e055
kekrby <kekrby@gmail.com>
2022-09-12 20:36:24 +0300

    t2-iso-minimal, t2-iso-gnome: switch to wpa_supplicant from iwd
    See https://github.com/kekrby/nixos-hardware/commit/8a8b616ad0755a61317ab85dd48a8001d8a44861 for the rationale.

--- nix/t2-iso-minimal.nix
+++ nix/t2-iso-minimal.nix
@@ -10,11 +10,6 @@
   # ZFS is broken and prevents building without this
   nixpkgs.config.allowBroken = true;
 
-  # iwd is preferred because wpa_supplicant does not work well (though wpa_cli seems to work fine for some reason)
-  # wpa_supplicant is enabled by a module imported by the installation CD module so it is turned off so that it does not conflict with iwd
-  networking.wireless.iwd.enable = true;
-  networking.wireless.enable = false;
-
   environment.systemPackages = with pkgs; [
     vim 
   ];
