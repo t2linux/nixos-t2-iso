e1e071adae4c8f6d7f4b61266c47bcb3029e3d69
kekrby <kekrby@gmail.com>
2022-10-04 20:30:05 +0300

    common.nix: use the new names of binary cache related settings

--- nix/common.nix
+++ nix/common.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 
 {
-  nix = {
-    binaryCaches = [
+  nix.settings = {
+    trusted-substituters = [
       "https://t2linux.cachix.org"
     ];
-    binaryCachePublicKeys = [
+    trusted-public-keys = [
       "t2linux.cachix.org-1:P733c5Gt1qTcxsm+Bae0renWnT8OLs0u9+yfaK2Bejw="
     ];
   };
